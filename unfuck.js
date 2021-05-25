cac25e8a2ab64511a8c3df7980dceb6fd33ac3eb
cassandra fox <cass@fox.mom>
2021-05-25 00:16:15 -0700

    initial commit

---
+++ unfuck.js
@@ -0,0 +1,21 @@
+const unfuck_expression = new RegExp(/&?s=\d+/, 'g');
+
+function unfuck(request) {
+	var unfuckedURL = new URL(request.url);
+	if (unfuckedURL.href.match(unfuck_expression) != null) {
+		unfuckedURL.search = unfuckedURL.search.replaceAll(unfuck_expression, "");
+		return {
+			redirectUrl: unfuckedURL.href
+		}
+	}
+};
+
+browser.webRequest.onBeforeRequest.addListener(
+	unfuck,
+	{
+		urls: ["*://*.twitter.com/*/status/*"],
+		types: ["main_frame"],
+		
+	},
+	["blocking"]
+);
